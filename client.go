5536239366e759cc781d08d3b02af28854c20838
Nicholas Wiersma <nick@wiersma.co.za>
2019-05-29 18:15:57 +0200

    Started to flesh out the pool

--- client.go
+++ client.go
@@ -4,26 +4,56 @@ import (
 	"context"
 	"errors"
 	"io"
+	"net"
 	"time"
 )
 
-type RequestWriter interface {
-	Write(io.Writer) error
-}
-
-type ResponseReader interface {
-	Read(r io.Reader) error
-}
-
 type ClientCodec interface {
-	Write(ctx context.Context, w RequestWriter) (read bool, err error)
-	Read(ctx context.Context, r ResponseReader) error
+	Write(ctx context.Context, w io.Writer) (read bool, err error)
+	Read(ctx context.Context, r io.Reader) error
 }
 
 type ClientCodecFactory func(conn Connection) ClientCodec
 
+type PoolOpts struct {
+	// Dialer is the dialer to use
+	Dialer net.Dialer
+
+	// IdleTimeout is the maximum duration that a connection will remain idle for.
+	IdleTimeout time.Duration
+}
+
+// Pool represents a connection pool
 type Pool interface {
 	Get(addr string) (Connection, error)
+
+	Put(Connection)
+}
+
+type pool struct {
+	fac         ClientCodecFactory
+	dialer      net.Dialer
+	idleTimeout time.Duration
+}
+
+func NewPool(fac ClientCodecFactory, opts PoolOpts) (Pool, error) {
+	if fac == nil {
+		return nil, errors.New("tcp: fac cannot be nil")
+	}
+
+	return &pool{
+		fac:         fac,
+		dialer:      opts.Dialer,
+		idleTimeout: opts.IdleTimeout,
+	}, nil
+}
+
+func (p *pool) Get(addr string) (Connection, error) {
+	return nil, nil
+}
+
+func (p *pool) Put(conn Connection) {
+
 }
 
 type ClientOpts struct {
@@ -32,26 +62,21 @@ type ClientOpts struct {
 }
 
 type Client struct {
-	fac  ClientCodecFactory
 	pool Pool
 }
 
-func NewClient(fac ClientCodecFactory, pool Pool, opts ClientOpts) (*Client, error) {
-	if fac == nil {
-		return nil, errors.New("tcp: fac cannot be nil")
-	}
-
+func NewClient(pool Pool, opts ClientOpts) (*Client, error) {
 	if pool == nil {
 		return nil, errors.New("tcp: pool cannot be nil")
 	}
 
 	return &Client{
-		fac:  fac,
+
 		pool: pool,
 	}, nil
 }
 
-func (c *Client) Send(ctx context.Context, w RequestWriter, r ResponseReader) error {
+func (c *Client) Send(ctx context.Context, w io.Writer, r io.Reader) error {
 	// All in a loop with checks for retry: conn could have been closed on idle, etc.
 
 	// Get connection+codec from pool
